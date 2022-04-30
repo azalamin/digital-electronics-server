68932ff7ab4c5e621f275707ebdf231b72aafd68
Al-amin Sheikh <azalamins@gmail.com>
2022-04-30 16:37:44 +0600

    inventory data api done

--- index.js
+++ index.js
@@ -9,6 +9,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qaeg0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client
+      .db("digitalElectronics")
+      .collection("inventories");
+    console.log("DB connected");
+
+    // Get inventories
+    app.get("/inventory", async(req, res) => {
+        const size = parseInt(req.query.size);
+        console.log(size);
+        const query = {};
+        let inventories;
+        if (size) {
+            inventories = await inventoryCollection.find(query).limit(size).toArray();
+        } else{
+            inventories = await inventoryCollection.find(query).toArray();
+        }
+        res.send(inventories)
+    });
+
+  } finally {
+    // client.close()
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Digital Electronics Server is running");
